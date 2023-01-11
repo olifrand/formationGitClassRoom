15fa4ebe8c3089fcc5e205d05f32ac944f2d3389
olifrand <olivier.frandon@csgroup.eu>
2023-01-11 15:26:03 +0100

    Revert "Clean"
    This reverts commit 39957d5a4e1d3f25246d8359186356c4698ee576.

--- toto.c
+++ toto.c
@@ -1,6 +1,14 @@
 essai
 ajout 3eme branche
+<<<<<<< HEAD
 essai stash
 modif sur main
+<<<<<<< HEAD
 
 gros caca
+=======
+gros caca
+=======
+essai stash
+>>>>>>> parent of 9a9a821 (Betise)
+>>>>>>> parent of 39957d5 (Clean)
